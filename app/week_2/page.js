72e84387fb32ccb94b2b4a554342f66a93a390f0
jashansran2581 <jashansran1969@gmail.com>
2024-05-16 19:45:08 -0400

    CPRG306 commit all changes

---
+++ app/week_2/page.js
@@ -0,0 +1,9 @@
+import StudentInfo from "./student-info"; 
+export default function Page() {
+    return (
+      <main className="bg-black text-white min-h-screen">
+        <h1>Shopping List</h1>
+        <StudentInfo/>
+      </main>
+    );
+  }
---
+++ app/week_2/student-info.js
@@ -0,0 +1,9 @@
+import Link from "next/link" 
+export default function StudentInfo() {
+    return (
+        <div className="">
+            <h1 className="">Jashanpreet Singh Sran</h1>
+            <Link href="https://github.com/jashansran2581/cprg306-assignments.git">Github.com</Link>
+        </div>
+    )
+  }
