ee00e353a45a6602d3c7a83cbd56dff9a9778125
jashansran2581 <jashansran1969@gmail.com>
2024-05-31 14:32:17 -0600

    Third commit

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function HomePage() {
             <h1 className="">CPRG 306: Web Development 2 - Assignments</h1>
             <div><Link href="/week_2">Week-2</Link></div>
             <div><Link href="/week-3">Week-3</Link></div>
+            <div><Link href="/week-4">Week-4</Link></div>
     </div> 
     </main>
   );
