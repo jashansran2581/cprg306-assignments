8c6f7b9577b4ffaabb174ccddcb850ac0ff04544
jashansran2581 <jashansran1969@gmail.com>
2024-07-13 13:40:52 -0600

    commit

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function HomePage() {
             <div><Link href="/week-5">Week-5</Link></div>
             <div><Link href="/week-6">Week-6</Link></div>
             <div><Link href="/week-7">Week-7</Link></div>
+            <div><Link href="/week-8">Week-8</Link></div>
     </div> 
     </main>
   );
